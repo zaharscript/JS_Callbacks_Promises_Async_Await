1b0dcb462d88698d54ee5542afffeaa7264387b1
Zaharudin Mokhtar <zahratulnazirah@gmail.com>
2023-07-27 00:19:42 +0800

    adding lesson until callback function.continue at promises

---
+++ callback.js
@@ -0,0 +1,25 @@
+const posts = [
+    {title: 'Post One', body: 'This is post one'},
+    {title: 'Post Two', body: 'This is post two'}
+];
+
+function getPosts() {
+    setTimeout(()=>{
+        let output = '';
+        posts.forEach((post,index)=>{
+            output += `<li>${index+1}. ${post.title}</li>`;
+        });
+        document.body.innerHTML = output;
+    },1000);
+}
+
+function  createPost(post, callback){
+    setTimeout(()=>{
+        posts.push(post)
+        callback();
+    },2000)
+}
+
+createPost({title: 'Post Three', body: 'This is post Three'},getPosts);
+
+
